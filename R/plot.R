fbd6f13ee195f8075c085c035c01acb1d4f401f1
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-02-21 16:24:24 +0000

    add custom history function
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R@40 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/plot.R
+++ R/plot.R
@@ -14,3 +14,16 @@ dmc.plotcorr <- function(dt) {
     my.plotcorr(corr, col=colors[((corr + 1)/2) * 100],
                 diag='ellipse', upper.panel="number", mar=c(0,2,0,0))
 }
+
+# Usage: dmc.hist(dt$weight)
+dmc.hist <- function(dat, ...) {
+    ds <- rbind(data.frame(dat=dat, grp="All"))
+    hs <- hist(dat, border=TRUE, ...)
+    dens <- density(dat, na.rm=TRUE)
+    rs <- max(hs$counts)/max(dens$y)
+    lines(dens$x, dens$y*rs, col="red")
+    legend("topright",legend=c("Histogram","Kernel Density Estimate"),lty=c(NA,1),
+           lwd=c(NA,2), pch = c(15, NA), col = c("grey80", "red"), merge=TRUE, 
+           inset=.01, cex=.8, adj=0)
+    rug(ds[ds$grp=="All",1])
+}
