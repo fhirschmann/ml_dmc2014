090c97cd02bd3e26a13c7fb12a24b8265039a633
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-02-21 16:24:18 +0000

    add plotting stuff
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R@36 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

---
+++ R/plot.R
@@ -0,0 +1,17 @@
+### Plotting Functions ###
+library(ellipse)
+source("R/lib/plotcorr.R")
+source("R/pp.R")
+
+# Usage: dmc.plotcorr(dt)
+dmc.plotcorr <- function(dt) {
+    dt.numeric <- dt[sapply(dt, is.numeric)]
+    colsc <- c(rgb(241, 54, 23, maxColorValue=255), 
+               'white', rgb(0, 61, 104, maxColorValue=255))
+    colramp <- colorRampPalette(colsc, space='Lab')
+    corr <- cor(dt.numeric,
+                use="all.obs", method="pearson")
+    colors = colramp(100)
+    my.plotcorr(corr, col=colors[((corr + 1)/2) * 100],
+                diag='ellipse', upper.panel="number", mar=c(0,2,0,0))
+}
