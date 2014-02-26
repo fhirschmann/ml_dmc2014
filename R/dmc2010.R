3be728c70da35a8583f19584e738d9269452c6f6
sholthausen <sholthausen@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-02-26 18:10:12 +0000

    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@72 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/dmc2010.R
+++ R/dmc2010.R
@@ -14,6 +14,28 @@ dmc.summary <- function (data, lev = NULL, model = NULL) {
     out <- twoClassSummary(data, lev, model)
     c(Points=dmc.points(data$pred, data$obs), out)
 }
-
-dmc.cost <- matrix(c(0, 5, 1.5, 0), 2, 2, byrow=TRUE)
+#Kostenmatrix:
+#ursprünglich:
+#			obs=1	obs=0
+#voucher=1	-5		1.5
+#voucher=0	0		0
+#
+#da voucher = !target90 ergibt sich
+#
+#			obs=1	obs=0
+#target90=1	0		0
+#target90=0	-5		1.5
+#
+#umformen für kostenmatrix (matrix will minimieren, nicht maximieren) ergibt
+#
+#			obs=1	obs=0
+#target90=1	0		0
+#target90=0	5		-1.5
+#
+#kostenmatrix muss auf der diagonalen 0 sein, ergibt:
+#
+#			obs=1	obs=0
+#target90=1	0		1.5
+#target90=0	5		0
+dmc.cost <- matrix(c(0, 1.5, 5, 0), 2, 2, byrow=TRUE)
 colnames(dmc.cost) <- rownames(dmc.cost) <- rev(c("no", "yes"))
