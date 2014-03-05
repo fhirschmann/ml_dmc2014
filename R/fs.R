0a787ba93e36481486cb6ec050b8d74e7118e104
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-03-05 11:04:10 +0000

    feature selection template
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@103 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

---
+++ R/fs.R
@@ -0,0 +1,21 @@
+fs.all <- function(dt) {
+    # Remove features not required at all
+    dt$customernumber <- NULL
+    dt
+}
+
+fs.c50 <- function(dt) {
+    dt2 <- fs.all(dt)
+
+    # C50 specific features
+    
+    dt2
+}
+
+fs.rpart <- function(dt) {
+    dt2 <- fs.all(dt)
+    
+    # rpart specific features
+    
+    dt2
+}
