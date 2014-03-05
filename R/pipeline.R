58a0faf2ff6f6a99d7d3145fef923fbac4e9d360
sholthausen <sholthausen@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-03-05 11:34:46 +0000

    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@107 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/pipeline.R
+++ R/pipeline.R
@@ -1,6 +1,7 @@
 # Training function definition
 
 source("R/pp.R")
+source("R/fs.R")
 source("R/dmc2010.R")
 
 suppressPackageStartupMessages(library(caret))
@@ -24,9 +25,9 @@ mytrain <- function(...) {
 # The list of training functions
 trainers <- list()
 #trainers$nb <- function() mytrain(method="nb", data=dt2, weights=weights)
-trainers$c50 <- function() mytrain(method="C5.0Cost", data=fs.c50, cost=dmc.cost,
+trainers$c50 <- function() mytrain(method="C5.0Cost", data=fs.c50(dt2), cost=dmc.cost,
                                    control=C5.0Control(earlyStopping=FALSE))
-trainers$cart <- function() mytrain(method="rpart", data=fs.cart,
+trainers$cart <- function() mytrain(method="rpart", data=fs.cart(dt2),
                                     # rpart wants the matrix to have true classes
                                     # in rows, so we transpose
                                     parms=list(loss=t(dmc.cost)))
