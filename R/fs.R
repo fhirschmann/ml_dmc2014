f0b3ce40177938e1f1c577f842143d363c075a0f
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-03-05 12:00:10 +0000

    remove more attributes
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@110 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/fs.R
+++ R/fs.R
@@ -2,10 +2,14 @@ fs.all <- function(dt) {
     dt2 <- dt
     # Remove features not required at all
     dt2$customernumber <- NULL
+    dt2$date <- NULL
     dt2$deliverydatereal <- NULL
     dt2$deliverydatepromised <- NULL
     dt2$datecreated <- NULL
     dt2$points <- NULL
+    dt2$advertisingdatacode <- NULL
+    dt2$delivpostcode <- NULL
+    dt2$deliverydatediff <- NULL
     dt2
 }
 
