6012beed82d1d2cabe4780928b3193a6899e3f94
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-03-07 01:53:16 +0000

    documented utils
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@137 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,17 @@
 # Utility functions
 
-as.binary <- function(x) {
+as.binary <- function(x, cond=1) {
+    # Makes an observation pseudo-binary, by assigning "yes" to the
+    # observation that meets the given requirement, and "no" otherwise.
+    # This is useful where logicals are not supported.
+    #
+    # Args:
+    #   x: a vector
+    #   cond: the condition to meet
+    #
+    # Returns:
+    #   a pseudo-binary factor
+    
     if (length(unique(x)) != 2) {
         stop("The given variable is not binary.")
     }
@@ -8,51 +19,66 @@ as.binary <- function(x) {
 }
 
 df.classes <- function(dt) {
+    # Prints a Data Frame's column classes
+    #
+    # Args:
+    #   dt: a data frame
+    
     df <- t(data.frame(lapply(dt, function(x) paste(class(x), collapse="/"))))
     colnames(df) <- c("class")
     df
 }
 
 df.nas <- function(dt) {
+    # Prints the number of NAs
+    #
+    # Args:
+    #   dt: a data frame
+    
     df <- t(data.frame(lapply(dt, function(x) sum(is.na(x)))))
     colnames(df) <- c("NAs")
     df
 }
 
 caret.bestidx <- function(fit) {
+    # Returns the index of the best tune observations and predictions
+    # for a caret fit.
+    #
+    # Args:
+    #   dt: a caret fit
+    
     params <- colnames(fit$bestTune)
     Reduce('&', lapply(params, function(y) fit$pred[[y]] == fit$bestTune[[y]]))
 }
 
 caret.pred <- function(fit) {
+    # Extracts the predictions from a caret fit.
+    #
+    # Args:
+    #   dt: a caret fit
+    
     fit$pred$pred[caret.bestidx(fit)]
 }
 
 caret.obs <- function(fit) {
+    # Extracts the observations from a caret fit.
+    #
+    # Args:
+    #   dt: a caret fit
+
     fit$pred$obs[caret.bestidx(fit)]
 }
 
 dmc.inst <- function(upgrade=F) {
-    libs <- c("devtools",
-              "Metrics",
-              "ellipse",
-              "data.table",
-              "knitr",
-              "mda",
-              "Hmisc",
-              "lubridate",
-              "earth",
-              "pROC",
-              "C50",
-              "ROCR",
-              "doMC",
-              "caret",
-              "chron",
-              "RWeka",
-              "klaR",
-              "plyr",
-              "MASS",
-              "ggplot2")
+    # Installs the required dependencies.
+    #
+    # Args:
+    #   upgrade: force upgrade
+
+    libs <- c("devtools", "Metrics", "ellipse", "data.table", "knitr",
+              "mda", "Hmisc", "lubridate", "earth", "pROC", "C50",
+              "ROCR", "doMC", "caret", "chron", "RWeka", "klaR",
+              "plyr", "MASS", "ggplot2")
     if (upgrade) {
         install.packages(libs)
     } else {
@@ -61,6 +87,11 @@ dmc.inst <- function(upgrade=F) {
 }
 
 dmc.load <- function(mdir="models") {
+    # Returns a list of serialized models.
+    #
+    # Args:
+    #   mdir: the directory to search for models in
+    
     models <- list()
     cwd <- getwd()
     setwd(mdir)
@@ -76,6 +107,12 @@ dmc.load <- function(mdir="models") {
 }
 
 dmc.run <- function(name) {
+    # Runs training for an algorithm and serializes the
+    # fitted model.
+    #
+    # Args:
+    #   name: the name of the training function
+
     fit <- trainers[[name]]()
     
     fname <- file.path("models", paste(name, ".RData", sep=""))
