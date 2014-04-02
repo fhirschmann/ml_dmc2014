73dd81e22b0452c2c747a94ae3efac81206296cb
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-02 10:46:09 +0000

    add new task
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@382 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/data.R
+++ R/data.R
@@ -4,13 +4,13 @@ source("R/utils.R")
 na.strings <- c("NA", "", "??", "?")
 
 # Lazy evaluation: dt and dt.test get constructed when needed
-delayedAssign("dt.raw", read.csv("task2010/dmc2010_train.txt",
+delayedAssign("dt.raw", read.csv("task/orders_train.txt",
                                  sep=";", na.strings=na.strings))
 delayedAssign("dt.unclean", pp(dt.raw))
 delayedAssign("dt", addlevels(im(cl(dt.unclean)), dt.test.unclean))
 
 
-delayedAssign("dt.test.raw", read.csv("task2010/dmc2010_class.txt",
+delayedAssign("dt.test.raw", read.csv("task/orders_class.txt",
                                       sep=";", na.strings=na.strings))
 delayedAssign("dt.test.unclean", pp(dt.test.raw))
 delayedAssign("dt.test", addlevels(im(cl(dt.test.unclean)), dt.unclean))
--- R/pp.R
+++ R/pp.R
@@ -18,64 +18,32 @@ pp <- function(dt) {
     ## Nominal Predictors    
     # Use labels instead of numeric values
     dt2_factors <- list(
-        "customernumber"=NULL,
-        "salutation"=c("0"="Ms.", "1"="Mr.", "2"="Company"),
-        "domain"=c("0"="aol.com", "1"="arcor.de", "2"="freenet.de", "3"="gmail.com",
-                   "4"="gmx.de", "5"="hotmail.de", "6"="online.de", "7"="onlinehome.de",
-                   "8"="t-online.de", "9"="web.de", "10"="yahoo.com", "11"="yahoo.de",
-                   "12"="other"),
-        "model"=NULL,
-        "paymenttype"=c("0"="invoice", "1"="cash", "2"="transfer_current", "3"="transfer_cc"),
-        "deliverytype"=c("0"="dispatch", "1"="collection"),
-        "invoicepostcode"=NULL,
-        "delivpostcode"=NULL,
-        "advertisingdatacode"=NULL,
-        "entry"=c("0"="shop", "1"="partner"),
-        "voucher"=c("0"="no", "1"="yes"),
-        "title"=c("0"="no", "1"="yes"),
-        "newsletter"=c("0"="no", "1"="yes"),
-        "gift"=c("0"="no", "1"="yes"),
-        "shippingcosts"=c("0"="no", "1"="yes")
+        "customerID"=NULL,
+        "manufacturerID"=NULL,
+        "itemID"=NULL,
+        "returnShipment"=c("0"="no", "1"="yes")
     )
 
     for (name in names(dt2_factors)) {
-        dt2[[name]] <- as.factor(dt2[[name]])
-        if (!is.null(dt2_factors[[name]]))
-            dt2[[name]] <- revalue(dt2[[name]], dt2_factors[[name]])
-    }
-    
-    if ("target90" %in% colnames(dt2)) {
-        dt2$target90 <- revalue(as.factor(dt2$target90), c("0"="no", "1"="yes"))
+        if (name %in% colnames(dt2)) {
+            dt2[[name]] <- as.factor(dt2[[name]])
+            if (!is.null(dt2_factors[[name]]))
+                dt2[[name]] <- revalue(dt2[[name]], dt2_factors[[name]])
+        }
     }
         
     ## Ordered Predictors
-    dt2_ordinal <- c("case")
-    dt2[dt2_ordinal] <- lapply(dt2[dt2_ordinal], as.ordered)
+    #dt2_ordinal <- c("size")
+    #dt2[dt2_ordinal] <- lapply(dt2[dt2_ordinal], as.ordered)
         
     ## Date Predictors
-    dt2_dates <- c("date", "datecreated", "deliverydatepromised", "deliverydatereal")
+    dt2_dates <- c("dateOfBirth", "creationDate", "orderDate", "deliveryDate")
     dt2[dt2_dates] <- lapply(dt2[dt2_dates], as.Date)
-
-    ## Fixes
-    # Because delivpostcode has strings in it (EN, Nl, NW) it gets read as factor.
-    # invoicepostcode has no strings it it, so it gets read as numeric. However,
-    # this causes the leading 0s from invoicepostcode to be removed, which is problematic
-    # later when we impute missing values in delivpostcode with invoicepostcode. So
-    # we add leading zeros in invoicepostcode
-    dt2$invoicepostcode <- as.factor(formatC(as.numeric(dt2$invoicepostcode),
-                                             width=2, format="d", flag="0"))
-
-    #dt2$invoiceeqdeliv <- ifelse(as.character(dt$invoicepostcode) == 
-    #                                 as.character(dt$delivpostcode) |
-    #                                 is.na(dt$delivpostcode), "yes", "no")
     
     # Add some features
-    dt2$deliverydatediff <- as.numeric(dt2$deliverydatepromised - dt2$deliverydatereal)
-    dt2$month <- as.factor(month(dt2$date))
-    dt2$weekday <- as.factor(wday(dt2$date, label=T, abbr=F))
+    #dt2$month <- as.factor(month(dt2$date))
+    #dt2$weekday <- as.factor(wday(dt2$date, label=T, abbr=F))
     
-    dt2$instantorder <- as.factor(ifelse(dt2$datecreated == dt2$date, "yes", "no"))
-
     #dt2$w0all <- Reduce("+", lapply(0:10, function(x) dt2[[paste("w", x, sep="")]]))
     
     dt2
@@ -91,27 +59,6 @@ cl <- function(dt) {
     #   A preprocessed data frame
 
     dt2 <- dt
-    #dt2[dt2$deliverydatepromised > as.Date("2011-01-01"),]$deliverydatepromised <- NA
-
-    # Outlier handling
-    ## Incorrect date in deliverydatepromised
-    outliers <- dt2$deliverydatepromised > as.Date("2011-01-01")
-    year(dt2[outliers,]$deliverydatepromised) <- year(dt[outliers,]$date)
-    dt2$deliverydatediff <- as.numeric(dt2$deliverydatepromised - dt2$deliverydatereal)
-    
-    ## Dates: See the slides for details
-    t <- 250
-    move <- dt2$deliverydatediff > t & !is.na(dt2$deliverydatediff)
-    dt2[move,]$deliverydatepromised <- dt2[move,]$deliverydatepromised - years(1)
-
-    ## wX: Slides, again
-    if (any(dt$w0 > 60))
-        dt2[dt2$w0 > 60,]$w0 <- mean(dt2[dt2$w0 < 60,]$w0)
-    if (any(dt$w1 > 60))
-    dt2[dt2$w1 > 60,]$w1 <- mean(dt2[dt2$w1 < 60,]$w1)
-    if (any(dt$w3) > 60)
-        dt2[dt2$w3 > 60,]$w3 <- mean(dt2[dt2$w3 < 60,]$w3)
-    
     dt2
 }
 
@@ -122,25 +69,5 @@ im <- function(dt) {
     #   dt: A data frame
     
     dt2 <- dt
-    
-    # Advertising Data Code: Just use the data code "NA" for missing values, because
-    # these values are probably missing because no advertising partner was used when
-    # making the purchases.
-    levels(dt2$advertisingdatacode) <- c(levels(dt2$advertisingdatacode), "NA")
-    dt2[is.na(dt2$advertisingdatacode),]$advertisingdatacode <- "NA"
-
-    # Impute missing values for delivpostcode with invoicepostcode
-    nas <- is.na(dt2$delivpostcode)
-    levels(dt2$delivpostcode) <- c(levels(dt2$delivpostcode),
-                                   levels(dt2$invoicepostcode))
-    dt2[nas,]$delivpostcode <- dt2[nas,]$invoicepostcode
-    
-    # Since this is just a warm-up, we'll use the median to impute the deliverydatediff
-    dt2[is.na(dt2$deliverydatediff),]$deliverydatediff <- round(median(dt2$deliverydatediff, na.rm=T), 0)
-
-    # And set the deliverydatereal attribute accordingly
-    nas <- is.na(dt2$deliverydatereal)
-    dt2[nas,]$deliverydatereal <- dt2[nas,]$deliverydatepromised + days(dt2[nas,]$deliverydatediff)
-
     dt2
 }
