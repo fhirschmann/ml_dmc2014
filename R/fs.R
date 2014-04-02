cc9a14a6d44c9a71ba4e615c3e6850f06244d95f
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-02 12:50:07 +0000

    remove date features in the pipeline
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@388 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/fs.R
+++ R/fs.R
@@ -1,4 +1,11 @@
 fs.all <- function(dt) {
     dt2 <- dt
+
+    dt2$customerID <- NULL
+    dt2$orderDate <- NULL
+    dt2$deliveryDate <- NULL
+    dt2$dateOfBirth <- NULL
+    dt2$creationDate <- NULL
+    
     dt2
 }
