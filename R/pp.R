be8cf3309a034db521a34532228b65132a536501
sholthausen <sholthausen@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-11 13:21:22 +0000

    feature "fewcolors" hinzugefügt. bitte nochmal drüberschauen sowohl hinsichtlich r-code als auch ob ihr mit den farbwahlen einverstanden seid!
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@539 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/pp.R
+++ R/pp.R
@@ -45,6 +45,97 @@ pp <- function(dt) {
 
     dt2$customerAge <- as.integer(2013 - year(dt2$dateOfBirth))
     
+    #summarize colors:
+    colors <- dt2$color
+    colors <- revalue(colors, c("dark denim"="black", #blue?
+                      "dark navy"="blue",
+                      "ash"="grey",
+                      "bordeaux"="red",
+                      "mahagoni"="red", #brown?
+                      "gold"="yellow",
+                      "dark oliv"="green",
+                      "striped"="striped",
+                      "anthracite"="black", #grey?
+                      "antique pink"="red",
+                      "floral"="other", #?
+                      "baltic blue"="blue",
+                      "nature"="other", #?
+                      "ancient"="other", #?
+                      "curry"="yellow",
+                      "torquoise"="blue",
+                      "navy"="blue",
+                      "brown"="brown",
+                      "aubergine"="red", #brown?
+                      "mocca"="brown", #grey?
+                      "blau"="blue",
+                      "basalt"="grey",
+                      "azure"="blue",
+                      "coral"="red", #yellow?
+                      "pallid"="grey", #?
+                      "petrol"="blue",
+                      "silver"="grey",
+                      "habana"="red", #?
+                      "darkblue"="blue",
+                      "beige"="yellow", #brown/grey?
+                      "mint"="green",
+                      "khaki"="brown", #yellow?
+                      "hibiscus"="red",
+                      "orange"="red", #yellow?
+                      "yellow"="yellow",
+                      "black"="black",
+                      "blue"="blue",
+                      "purple"="red",
+                      "almond"="brown", #yellow?
+                      "red"="red",
+                      "berry"="red",
+                      "grey"="grey",
+                      "ocher"="brown",
+                      "magenta"="red",
+                      "olive"="green",
+                      "white"="white",
+                      "denim"="other", #?
+                      "pink"="red",
+                      "stained"="brown",
+                      "kanel"="brown", #red?
+                      "green"="green",
+                      "jade"="green",
+                      "aquamarine"="blue",
+                      "aqua"="blue",
+                      "ecru"="grey", #brown/yellow?
+                      "iron"="grey",
+                      "fuchsia"="red",
+                      "ingwer"="red", #brown?
+                      "cognac"="brown",
+                      "terracotta"="brown", #red?
+                      "apricot"="yellow",
+                      "graphite"="grey",
+                      "crimson"="red",
+                      "lemon"="yellow",
+                      "oliv"="green",
+                      "leopard"="yellow", #brown/other?
+                      "amethyst"="red",
+                      "aviator"="other", #?
+                      "bronze"="brown",
+                      "brwon"="brown",
+                      "caramel"="yellow",
+                      "champagner"="yellow", #grey?
+                      "cobalt blue"="blue",
+                      "copper coin"="brown",
+                      "cortina mocca"="brown",
+                      "creme"="yellow",
+                      "curled"="other", #?
+                      "currant purple"="red",
+                      "dark garnet"="black", #red?
+                      "dark grey"="grey",
+                      "ebony"="black",
+                      "ivory"="white",
+                      "mango"="yellow",
+                      "opal"="other", #kann alles sein
+                      "perlmutt"="other", #?
+                      "vanille"="white"))
+    dt2$fewcolors <- colors
+    
+    
     #dt2$w0all <- Reduce("+", lapply(0:10, function(x) dt2[[paste("w", x, sep="")]]))
     
     dt2
