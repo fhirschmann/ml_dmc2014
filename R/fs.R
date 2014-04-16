8992c48bef69e63cc81d0c3864b09839a8e504f3
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-16 13:51:04 +0000

    update feature selection
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@584 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/fs.R
+++ R/fs.R
@@ -1,13 +1,15 @@
 fs.all <- function(dt) {
     dt2 <- dt
 
-    dt2$customerID <- NULL
+    #dt2$customerID <- NULL
     dt2$orderDate <- NULL
     dt2$deliveryDate <- NULL
     dt2$dateOfBirth <- NULL
     dt2$creationDate <- NULL
-    dt2$itemID <- NULL
-    dt2$manufacturerID <- NULL
+    #dt2$itemID <- NULL
+    #dt2$manufacturerID <- NULL
+    dt2$colors <- NULL
+    dt2$instantorder <- NULL
     
     dt2$orderItemID <- NULL  # This attribute is useless
     
