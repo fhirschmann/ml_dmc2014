c99a7a0d57f155f1b112cd5587b61c0f308b2a42
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-21 13:33:10 +0000

    update fix missing
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@624 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/fix-missing.R
+++ bin/fix-missing.R
@@ -29,12 +29,9 @@ dt.customer$customerID <- as.numeric(dt.customer$customerID)
 dt.customer$creationDate <- as.numeric(dt.customer$creationDate)
 dt.customer$firstOrderDate <- as.numeric(dt.customer$firstOrderDate)
 
-
-
 dt.customer.samp <- dt.customer[sample(nrow(dt.customer), nrow(dt.customer) / 10), ]
 
 set.seed(100)
-
 fit <- train(creationDate ~ .,
              method="cubist",
              data=dt.customer.samp[c("customerID", "creationDate", "firstOrderDate",
@@ -45,3 +42,36 @@ fit <- train(creationDate ~ .,
                  method="cv",
                  summaryFunction=function(data, lev=NULL, model=NULL) c(MAE=mae(data$obs, data$pred))))
 fit
+
+# fix customerAge
+
+dt.age <- dt.merged[c("customerID", "customerAge", "fewcolors", "orderDate", "price")]
+dt.age.tbl <- data.table(dt.age)
+dt.age$numberOfItemsOrdered <- dt.age.tbl[,x := .N, by=c("customerID")]$x
+#dt.age$numberOfOrders <- dt.age.tbl[, x := .N, by=c("customerID", "orderDate")]$x
+dt.age$orderVolume <- dt.age.tbl[, x := as.integer(sum(price)), by=c("customerID")]$x
+dt.age$favoriteColor <- names(which.max(prop.table(table(dt.age$fewcolors))))
+dt.age$favoriteColor <- as.factor(dt.age$favoriteColor)
+dt.age$leastFavoriteColor <- names(which.min(prop.table(table(dt.age$fewcolors))))
+dt.age$leastFavoriteColor <- as.factor(dt.age$leastFavoriteColor)
+
+dt.age$fewcolors <- NULL
+dt.age$orderDate <- NULL
+dt.age$price <- NULL
+
+dt.age <- unique(dt.age[!is.na(dt.age$customerAge), ])
+
+dt.age.samp <- dt.age[sample(nrow(dt.age), nrow(dt.age) / 10), ]
+
+set.seed(100)
+fit <- train(customerAge ~ .,
+             method="cubist",
+             data=dt.age,
+             metric="MAE",
+             tuneGrid=expand.grid(.committees=1, .neighbors=0),
+             trControl=trainControl(
+                 method="cv",
+                 summaryFunction=function(data, lev=NULL, model=NULL) c(MAE=mae(data$obs, data$pred))))
+fit
+
+
