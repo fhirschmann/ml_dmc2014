52dd696ce17fb99b3294518028bf6a7c384eeb54
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-23 12:38:18 +0000

    fix pipeline again
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@688 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

---
+++ bin/make-data.R
@@ -0,0 +1,169 @@
+#!/usr/bin/env Rscript
+library(data.table)
+library(plyr)
+library(lubridate)
+library(vcd)
+library(zoo)
+
+# Read in the Data
+
+dt.na.strings <- c("NA", "", "??", "?")
+
+dt.classes <- c(
+    "orderItemID"="factor",
+    "orderDate"="Date",
+    "deliveryDate"="Date",
+    "dateOfBirth"="Date",
+    "itemID"="factor",
+    "manufacturerID"="factor",
+    "customerID"="factor",
+    "creationDate"="Date"
+)
+
+dt.train <- read.csv("task/orders_train.txt", sep=";",
+                     colClasses=dt.classes, na.strings=dt.na.strings)
+dt.train$returnShipment <- revalue(as.factor(dt.train$returnShipment), c("0"="no", "1"="yes"))
+dt.test <- read.csv("task/orders_class.txt", sep=";",
+                    colClasses=dt.classes, na.strings=dt.na.strings)
+
+# Feature Engineering
+
+## Customer Blacklist
+x <- as.data.frame.matrix(structable(returnShipment ~ customerID, data = dt.train))
+z <- dt.train[dt.train$customerID %in% rownames(x[x[["0"]] == 0, ]), c("customerID", 
+                                                                       "orderDate")]
+z <- z[!duplicated(z), ]
+zz <- as.data.frame(table(z$customerID))
+
+#ideen für features:
+#größe des customers aus bestellen items ermitteln
+#preisfeature: abweichung von preis (höher, niedriger, gleich)
+#anrede: runterbrechen auf "männlich" / "weiblich" (falls kein großer unterschied bei returnshipments zwischen anreden)
+
+dt.train$returnShipment <- revalue(dt.train$returnShipment, c("0"="no", "1"="yes"))
+
+feat.simple <- function(dt) {
+    dt2 <- data.table(dt)
+    
+    #dt2$creationDateMissing <- as.factor(ifelse(is.na(dt2$creationDate), "yes", "no"))
+    
+    dt2$deliveryTime <- as.integer(dt2$deliveryDate - dt2$orderDate)
+    dt2$deliveryDateMissing <- as.factor(ifelse(is.na(dt2$deliveryDate), "yes", "no"))
+    dt2$orderWeekday <- as.ordered(as.factor(lubridate::wday(dt2$orderDate, label=T, abbr=F)))
+    
+    # Customer age in Years
+    dt2$customerAge <- as.integer(year(dt2$orderDate) - year(dt2$dateOfBirth))
+    dt2$dateOfBirthMissing <- as.factor(ifelse(is.na(dt2$dateOfBirth), "yes", "no"))
+    
+    # Account age in Days
+    dt2$accountAge <- as.numeric(dt2$orderDate - dt2$creationDate)
+    
+    # Number of items ordered with the same ID
+    dt2 <- dt2[, sameItemsOrdered := .N, by=c("itemID", "customerID", "orderDate")]
+    
+    # Date of first order (per customer)
+    dt2 <- dt2[, firstOrderDate := min(orderDate), by=c("customerID")]
+    dt2$firstOrderDate <- as.Date(dt2$firstOrderDate)
+    
+    # Volume of order
+    dt2 <- dt2[, orderVolume := as.integer(sum(price)), by=c("customerID", "orderDate")]
+    
+    # Total volume of customer's order
+    dt2 <- dt2[, totalOrderVolume := as.integer(sum(price)), by=c("customerID")]
+    
+    # Summarize colors:
+    dt2$fewcolors <- revalue(dt2$color,
+                             c("dark denim"="black", #blue?
+                                "dark navy"="blue",
+                                "ash"="grey",
+                                "bordeaux"="red",
+                                "mahagoni"="red", #brown?
+                                "gold"="yellow",
+                                "dark oliv"="green",
+                                "striped"="other",
+                                "anthracite"="black", #grey?
+                                "antique pink"="red",
+                                "floral"="other", #?
+                                "baltic blue"="blue",
+                                "nature"="other", #?
+                                "ancient"="other", #?
+                                "curry"="yellow",
+                                "turquoise"="blue",
+                                "navy"="blue",
+                                "brown"="brown",
+                                "aubergine"="red", #brown?
+                                "mocca"="brown", #grey?
+                                "blau"="blue",
+                                "basalt"="grey",
+                                "azure"="blue",
+                                "coral"="red", #yellow?
+                                "pallid"="grey", #?
+                                "petrol"="blue",
+                                "silver"="grey",
+                                "habana"="red", #?
+                                "darkblue"="blue",
+                                "beige"="yellow", #brown/grey?
+                                "mint"="green",
+                                "khaki"="brown", #yellow?
+                                "hibiscus"="red",
+                                "orange"="red", #yellow?
+                                "yellow"="yellow",
+                                "black"="black",
+                                "blue"="blue",
+                                "purple"="red",
+                                "almond"="brown", #yellow?
+                                "red"="red",
+                                "berry"="red",
+                                "grey"="grey",
+                                "ocher"="brown",
+                                "avocado"="green",
+                                "magenta"="red",
+                                "olive"="green",
+                                "white"="white",
+                                "denim"="other", #?
+                                "pink"="red",
+                                "stained"="brown",
+                                "kanel"="brown", #red?
+                                "green"="green",
+                                "jade"="green",
+                                "aquamarine"="blue",
+                                "aqua"="blue",
+                                "ecru"="grey", #brown/yellow?
+                                "iron"="grey",
+                                "fuchsia"="red",
+                                "ingwer"="red", #brown?
+                                "cognac"="brown",
+                                "terracotta"="brown", #red?
+                                "apricot"="yellow",
+                                "graphite"="grey",
+                                "crimson"="red",
+                                "lemon"="yellow",
+                                "oliv"="green",
+                                "leopard"="yellow", #brown/other?
+                                "amethyst"="red",
+                                "aviator"="other", #?
+                                "bronze"="brown",
+                                "brwon"="brown",
+                                "caramel"="yellow",
+                                "champagner"="yellow", #grey?
+                                "cobalt blue"="blue",
+                                "copper coin"="brown",
+                                "cortina mocca"="brown",
+                                "creme"="yellow",
+                                "curled"="other", #?
+                                "currant purple"="red",
+                                "dark garnet"="black", #red?
+                                "dark grey"="grey",
+                                "ebony"="black",
+                                "ivory"="white",
+                                "mango"="yellow",
+                                "opal"="other", #kann alles sein
+                                "perlmutt"="other", #?
+                                "vanille"="white"))
+    dt2
+}
+
+dt.train <- feat.simple(dt.train)
+dt.test <- feat.simple(dt.test)
+
+save(dt.train, dt.test, file="data.RData")
