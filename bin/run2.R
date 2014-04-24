35d00db5f03903a1f61734f5125903535e4b91fd
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-24 18:39:31 +0000

    use different fs function
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@709 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/run2.R
+++ bin/run2.R
@@ -3,4 +3,4 @@ source("R/data.R")
 source("R/fs.R")
 library(C50)
 
-fit.c50.t1 <- C5.0(returnShipment ~ ., rules=TRUE, data=fs.all(dt.t1$train))
+fit.c50.t1 <- C5.0(returnShipment ~ ., rules=TRUE, data=fs.tree(dt.t1$train))
