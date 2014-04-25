6200b0c1115bf65d23d5212c7e2c6f73940f1210
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-25 21:31:37 +0000

    move features to feat.R
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@735 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/make-data.R
+++ bin/make-data.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(vcd)
 library(zoo)
 
-source("R/colormap.R")
+source("R/feat.R")
 
 # Read in the Data
 dt.na.strings <- c("NA", "", "??", "?")
@@ -43,67 +43,6 @@ zz <- as.data.frame(table(z$customerID))
 #anrede: runterbrechen auf "männlich" / "weiblich" (falls kein großer unterschied bei returnshipments zwischen anreden)
 
 dt.train$returnShipment <- revalue(dt.train$returnShipment, c("0"="no", "1"="yes"))
-
-add.features <- function(dt) {
-    dt2 <- data.table(dt)
-    
-    #dt2$creationDateMissing <- as.factor(ifelse(is.na(dt2$creationDate), "yes", "no"))
-    
-    dt2$deliveryTime <- as.integer(dt2$deliveryDate - dt2$orderDate)
-    dt2$deliveryDateMissing <- as.factor(ifelse(is.na(dt2$deliveryDate), "yes", "no"))
-    dt2$orderWeekday <- as.ordered(as.factor(lubridate::wday(dt2$orderDate, label=T, abbr=F)))
-    
-    # Customer age in Years
-    dt2$customerAge <- as.integer(year(dt2$orderDate) - year(dt2$dateOfBirth))
-    dt2$dateOfBirthMissing <- as.factor(ifelse(is.na(dt2$dateOfBirth), "yes", "no"))
-    
-    # Account age in Days
-    dt2$accountAge <- as.numeric(dt2$orderDate - dt2$creationDate)
-    
-    # Number of items ordered with the same ID
-    dt2 <- dt2[, sameItemsOrdered := .N, by=c("itemID", "customerID", "orderDate")]
-    
-    # Date of first order (per customer)
-    dt2 <- dt2[, firstOrderDate := min(orderDate), by=c("customerID")]
-    dt2$firstOrderDate <- as.Date(dt2$firstOrderDate)
-    
-    # Volume of order
-    dt2 <- dt2[, orderVolume := sum(price), by=c("customerID", "orderDate")]
-    
-    # Total volume of customer's order
-    dt2 <- dt2[, totalOrderVolume := sum(price), by=c("customerID")]
-    
-    # Summarize colors
-    dt2$fewcolors <- revalue(dt2$color, colormap)
-    
-    # Fix sizes
-    dt2$size <- droplevels(as.factor(toupper(dt2$size)))
-    
-    # Discretized price
-    dt2$discretizedPrice <- cut(dt2$price, c(0, 1:20 * 10, Inf), left=T, right=F)
-    
-    # West/East Germany
-    dt2$westGermany <- revalue(dt2$state, c(
-        "Baden-Wuerttemberg"="yes",
-        "Bavaria"="yes",
-        "Berlin"="no",
-        "Brandenburg"="no",
-        "Bremen"="yes",
-        "Hesse"="yes",
-        "Hamburg"="yes",
-        "Lower Saxony"="yes",
-        "Mecklenburg-Western Pomerania"="no",
-        "North Rhine-Westphalia"="yes",
-        "Rhineland-Palatinate"="yes",
-        "Schleswig-Holstein"="yes",
-        "Saarland"="yes",
-        "Saxony"="no",
-        "Saxony-Anhalt"="no",
-        "Thuringia"="no"
-    ))
-    
-    dt2
-}
 
 dt.train <- add.features(dt.train)
 dt.test <- add.features(dt.test)
