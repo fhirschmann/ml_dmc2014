b49fb0fc29fd295928fe95d8ad0f084644cba482
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-29 20:43:19 +0000

    new pipeline
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@812 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/dmc.R
+++ R/dmc.R
@@ -1,13 +1,122 @@
 # DMC2014 Specific Stuff
 source("R/feat.R")
+source("R/utils.R")
+
+dmc.train <- function(data, tuneGrid=NULL, fs.fun, verbose=T, method="nb", ...) {
+    require(caret)
+    
+    res <- list()
+    class(res) <- "dmctrain"
+    
+    if (is.null(tuneGrid)) {
+        # TODO
+    }
+    
+    res <- foreach(dt.name=names(data)) %do% {
+        results <- tuneGrid
+        models <- foreach(e=1:nrow(tuneGrid)) %do% {
+            if (verbose)
+                message(paste("Learning model for", method, "on", dt.name,
+                              e, "/", nrow(tuneGrid)))
+            fit <- caret::train(returnShipment ~ .,
+                                data=fs.fun(data[[dt.name]]$train),
+                                tuneGrid=tuneGrid[e, ],
+                                trControl=trainControl(
+                                    method="none"),
+                                method=method,
+                                ...)
+            preds <- predict(fit, fs.fun(data[[dt.name]]$test), na.action=na.pass)
+            score <- dmc.points(preds, data[[dt.name]]$test$returnShipment)
+            list(fit=fit, score=score, accuracy=1 - (score / nrow(data[[dt.name]]$test)), preds=preds)
+        }
+        results$score <- sapply(models, function(x) x$score)
+        results$accuracy <- sapply(models, function(x) x$accuracy)
+        list(models=models, results=results)
+    }
+    names(res) <- names(data)
+    
+    res
+}
+
+summary.dmctrain <- function(dmctrain) {
+    results <- sapply(names(dmctrain), function(n) data.frame(dmctrain[[n]]$results, set=n),
+                      simplify=F)
+    tbl <- do.call(rbind, results)
+    rownames(tbl) <- NULL
+    tbl
+}
 
 
-dmc.dtmap <- function(l, prefix) {
-    require(plyr)
-    l2 <- l
-    map <- c("T1"="01", "T2"="02", "T3"="03")
-    names(l2) <- paste(prefix, revalue(names(l), map), sep="")
-    l2
+dmc.multitrain <- function(form, data, method, fs.fun=identity,
+                           trControl=trainControl(), save.path=NA, 
+                           verbose=F, ...) {
+    # Trains a model using multiple train/test splits.
+    # Args:
+    #   form: formula
+    #   data: data, list(T1=list(train=dt.test, test=dt.test))
+    #   method: method for caret::train
+    #   fs.fun: feature selection function
+    #   trControl: caret::trainControl
+    #   save.path: filename stem to save to
+    #   ...: passed to dmc.train
+    #
+    # Returns:
+    #   a dmc.multitrain object
+    
+    require(foreach)
+    
+    models <- foreach(dt.name=names(data)) %do% {
+        message(paste("Learning model", method, "on", dt.name))
+        dmc.train(form=form, 
+                  dt.train=data[[dt.name]]$train,
+                  dt.test=data[[dt.name]]$test,
+                  trControl=trControl,
+                  save.path=save.path,
+                  method=method,
+                  save.fname=paste(dt.name, method, sep="_"),
+                  verbose=verbose,
+                  ...)
+    }
+    names(models) <- names(dt.dmc)
+    
+    res <- list(models=models)
+    class(res) <- "dmc.multitrain"
+    res
+}
+
+dmc.supertrain.default.desc <- list(train.args=list(),
+                                    data.fun=identity,
+                                    save.path="models")
+
+dmc.supertrain <- function(descs, common.desc=(d <- caret.train.default.desc),
+                           train.only=NULL) {
+    # Takes a list of learner descriptions and fits multiple models.
+    #
+    # Args:
+    #   descs: learner description
+    #   common.desc: description common to all learners
+    #   train.only: train only a subset of learners
+    #
+    # Returns:
+    #   a list of dmc.multifit objects
+    
+    fits <- list()
+    
+    for (name in (if (length(train.only) == 0) names(descs) else train.only)) {
+        train.args <- list.update(common.desc$train.args, descs[[name]]$train.args)
+        desc <- list.update(common.desc, descs[[name]])
+        desc$train.args <- train.args
+    
+        fit <- do.call(dmc.multitrain, c(desc$train.args,
+                                         fs.fun=desc$fs.fun,
+                                         trControl=desc$train.args$trControl,
+                                         save.path=desc$save.path,
+                                         verbose=desc$verbose))
+        
+        fits[[name]] <- fit
+    }
+    
+    fits
 }
 
 DmcTrain <- function(method, data=dt.dmc, fs.fun=identity, ...) {
