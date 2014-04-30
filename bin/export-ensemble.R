ebd086f38c5d3b2f5eda94d2e54163ac148008a4
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-04-30 20:05:23 +0000

    update export script
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@873 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/export-ensemble.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-source("R/dmc.R")
-args <- commandArgs(T)
-
-fit <- readRDS(args[[1]])
-preds <- extractPreds.dmctrain(fit)
-write.table(preds, file=args[[2]], quote=F, sep=";",
-            row.names=F)
--- bin/export-predictions.R
+++ bin/export-predictions.R
@@ -1,14 +1,26 @@
 #!/usr/bin/env Rscript
+#
+# Usage: ./bin/export-predictions.R models/C5.0.RData directory/
+#        ./bin/export-predictions.R models/C5.0.RData file.txt
 
 source("R/dmc.R")
+library(stringr)
+
 args <- commandArgs(T)
 
 fit <- readRDS(args[[1]])
-preds <- extractPreds.dmcmtrain(fit)
+preds <- sapply(extractPreds.dmcmtrain(fit),
+                function(x) {
+                    x$prediction <- dmc.convertPreds(x$prediction)
+                    x
+                }, simplify=F)
 
-for (n in names(preds)) {
-    pred <- preds[[n]]
-    pred$prediction <- dmc.convertPreds(pred$prediction)
-    write.table(pred, file=file.path(args[[2]], paste(n, "_pred.txt", sep="")),
-                quote=F, row.names=F, sep=";")
+if (str_sub(args[[2]], -4, -1) == ".txt") {
+    pred <- do.call(rbind, preds)
+    write.table(pred, file=args[[2]], quote=F, row.names=F, sep=";")
+} else {
+    for (n in names(preds)) {
+        write.table(preds[[n]], file=file.path(args[[2]], paste(n, "_pred.txt", sep="")),
+                    quote=F, row.names=F, sep=";")   
+    }
 }
