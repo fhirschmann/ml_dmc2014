cf9923d4103687959817486c0289f819c72d60d0
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-02 18:28:47 +0000

    add rf test
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@947 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

---
+++ bin/test-rf.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+source("R/data.R")
+source("R/fs.R")
+source("R/dmc.R")
+library(caret)
+
+dt.train <- dt.dmc$T1$train
+dt.test <- dt.dmc$T1$test
+
+use <- dt.train$deliveryDateMissing == "no"
+
+fit <- train(returnShipment ~ ., fs.rf(dt.train[use, ]),
+             method="rf", tuneLength=4,
+             na.action=na.pass, trControl=trainControl(method="cv", number=10))
+
+
+pred <- dt.test
+pred$pred <- predict(fit, dt.test, na.action=na.pass)
+pred[pred$deliveryDateMissing == "yes", ]$pred <- "no"
+
+score <- dmc.score(pred$pred, pred$returnShipment)
+score
+
+acc <- 1 - score / nrow(dt.test)
+acc
