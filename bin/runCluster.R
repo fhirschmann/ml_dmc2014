2079cc65e9cb23a1c1d6018b78d80e04229ddccd
sholthausen <sholthausen@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-04 14:53:21 +0000

    run cluster für clusterusage
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@981 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

---
+++ bin/runCluster.R
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+#
+# This runs the pipeline.
+#
+# You can give specify the models to train by giving
+# its names as arguments, e.g. ./bin/run.R c50 rf
+#
+# To get a list of available models try ./bin/run.R -l
+
+source("R/dmc.R")
+source("R/pipeline.R")
+source("R/utils.R")
+library(doParallel)
+registerDoParallel(4)
+
+args <- commandArgs(T)
+
+# Set the description for the learner
+desc <- list.update(common.desc, descs[[args[[1]]]])
+
+desc$data <- readRDS("data.dmc.RData")[[args[[2]]]]
+
+if ("-m" %in% args) {
+    desc$data <- list(train=head(desc$data$train, 2000),
+                      test=head(desc$data$test, 100))
+}
+
+desc$data.name <- args[[2]]
+
+#sapply(ls(), function(x) {message(x); object.size(get(x))})
+
+train <- do.call(dmctrain, desc)
+train[c("results", "bestResults")]
