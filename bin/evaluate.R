60bf2a5cbd1cea90271c19679885196b7e1abe5f
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-04 15:22:24 +0000

    interim evaluation script
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@985 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/evaluate.R
+++ bin/evaluate.R
@@ -1,8 +1,14 @@
 #!/usr/bin/Rscript
 #
-# Usage: (Rscript) bin/evaluate.R models/
+# Usage: (Rscript) bin/evaluate.R models
 source("R/dmc.R")
 
 args <- commandArgs(T)
+dir <- args[[1]]
+files <- list.files(path=dir, pattern=paste(pattern=".*_*._res.RData", sep=""))
 
-dmc.loadeva(args[[1]])
+res <- sapply(files, function(x) readRDS(file.path(dir, x)), simplify=F)
+for (n in names(res)) {
+    # TODO
+}
+res
