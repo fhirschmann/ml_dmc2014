396043a0f7cced4dbb10adc67116e7d952be997d
sholthausen <sholthausen@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-06 22:10:25 +0000

    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1109 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/pipeline.R
+++ R/pipeline.R
@@ -14,198 +14,198 @@ ctrl.probs <- ctrl
 ctrl.probs$classProbs <- TRUE
 
 grid.gbm <- expand.grid(
-    shrinkage=c(0.1),
-    interaction.depth=1:15,
-    n.trees=c(1, 1:100 * 5)
+  shrinkage=c(0.1),
+  interaction.depth=1:15,
+  n.trees=c(1, 1:100 * 5)
 )
 
 grid.c50 <- expand.grid(
-    model=c("rules", "tree"),
-    winnow=c(TRUE, FALSE),
-    trials=1:100
+  model=c("rules", "tree"),
+  winnow=c(TRUE, FALSE),
+  trials=1:100
 )
 
 # List of stuff to learn
 descs <- list(
-    # Stuff to run on the Cluster: Postfix with 'C'
-    
-    # GBM
-    gbmC=list(
-        fs.fun=fs.gbm,
-        method="gbm",
-        tuneGrid=grid.gbm
+  # Stuff to run on the Cluster: Postfix with 'C'
+  
+  # GBM
+  gbmC=list(
+    fs.fun=fs.gbm,
+    method="gbm",
+    tuneGrid=grid.gbm
+  ),
+  
+  gbmCsimon=list(
+    fs.fun=fs.simon,
+    method="gbm",
+    tuneGrid=grid.gbm
+  ),
+  
+  # GBM with Probabilities
+  gbmCP=list(
+    fs.fun=fs.gbm,
+    method="gbm",
+    tuneGrid=grid.gbm,
+    trControl=ctrl.probs
+  ),
+  
+  # C5.0
+  c50C=list(
+    fs.fun=fs.tree,
+    method="C5.0",
+    tuneGrid=grid.c50,
+    control=C5.0Control(earlyStopping=F)
+  ),
+  
+  # C5.0 with Probabilities
+  c50CP=list(
+    fs.fun=fs.tree,
+    method="C5.0",
+    tuneGrid=grid.c50,
+    trControl=ctrl.probs,
+    control=C5.0Control(earlyStopping=F)
+  ),
+  
+  # Penalized Multinomial Regression
+  mnC=list(
+    fs.fun=fs.rf,
+    method="multinom",
+    tuneLength=20
+  ),
+  
+  # Regularized Random Forest (Global)
+  rrfGC=list(
+    fs.fun=fs.rf,
+    method="RRFglobal",
+    tuneLength=8
+  ),
+  
+  # Support Vector Machine (Polynomial Kernel)
+  svmPolyC=list(
+    fs.fun=fs.rf,
+    method="svmPoly",
+    tuneLength=7
+  ),
+  
+  # Bagged Cart
+  treebagC=list(
+    fs.fun=fs.tree,
+    method="treebag"
+  ),
+  fda=list(
+    fs.fun=fs.nn,
+    method="fda"
+  ),
+  gbm=list(
+    fs.fun=fs.gbm,
+    method="gbm",
+    tuneLength=4,
+    tuneGrid=expand.grid(
+      shrinkage=0.001,
+      interaction.depth=5,
+      n.trees=100
     ),
-    
-    gbmCsimon=list(
-      fs.fun=fs.simon,
-      method="gbm",
-      tuneGrid=grid.gbm
-    ),
-    
-    # GBM with Probabilities
-    gbmCP=list(
-        fs.fun=fs.gbm,
-        method="gbm",
-        tuneGrid=grid.gbm,
-        trControl=ctrl.probs
-    ),
-    
-    # C5.0
-    c50C=list(
-        fs.fun=fs.tree,
-        method="C5.0",
-        tuneGrid=grid.c50,
-        control=C5.0Control(earlyStopping=F)
-    ),
-    
-    # C5.0 with Probabilities
-    c50CP=list(
-        fs.fun=fs.tree,
-        method="C5.0",
-        tuneGrid=grid.c50,
-        trControl=ctrl.probs,
-        control=C5.0Control(earlyStopping=F)
-    ),
-    
-    # Penalized Multinomial Regression
-    mnC=list(
-        fs.fun=fs.rf,
-        method="multinom",
-        tuneLength=20
-    ),
-    
-    # Regularized Random Forest (Global)
-    rrfGC=list(
-        fs.fun=fs.rf,
-        method="RRFglobal",
-        tuneLength=8
-    ),
-    
-    # Support Vector Machine (Polynomial Kernel)
-    svmPolyC=list(
-        fs.fun=fs.rf,
-        method="svmPoly",
-        tuneLength=7
-    ),
-    
-    # Bagged Cart
-    treebagC=list(
-        fs.fun=fs.tree,
-        method="treebag"
-    ),
-    fda=list(
-        fs.fun=fs.nn,
-        method="fda"
-    ),
-    gbm=list(
-        fs.fun=fs.gbm,
-        method="gbm",
-        tuneLength=4,
-        tuneGrid=expand.grid(
-          shrinkage=0.001,
-          interaction.depth=5,
-          n.trees=100
-        ),
-        n.minobsinnode=3
-    ),
-    mlp=list(
-        fs.fun=fs.rf,
-        method="mlp"
-    ),
-    mda=list(
-        fs.fun=fs.rf,
-        method="mda"
-    ),
-    lmt=list(
-        fs.fun=fs.tree,
-        method="LMT"
-    ),
-    lboost=list(
-        fs.fun=fs.rf,
-        method="LogitBoost"
-    ),
-    j48=list(
-        fs.fun=fs.tree,
-        method="J48"
-    ),
-    jrip=list(
-        fs.fun=fs.tree,
-        method="JRip"
-    ),
-    svmLinear=list(
-        fs.fun=fs.svm,
-        method="svmLinear"
-    ),
-    rf=list(
-        fs.fun=fs.rf,
-        method="rf",
-        tuneLength=2
-    ),
-    rfC=list(
-      fs.fun=fs.rf,
-      method="rf",
-      tuneLength=15
-    ),
-    rf2=list(
-        fs.fun=fs.rf,
-        method="extraTrees",
-        tuneLength=1
-    ),
-    orf=list(
-        fs.fun=fs.rf,
-        method="ORFridge"
-    ),
-    rrf=list(
-        fs.fun=fs.rf,
-        method="RRFglobal",
-        tuneLength=3
-    ),
-    treebag=list(
-        fs.fun=fs.rf,
-        method="treebag"
-    ),
-    svmLinear=list(
-        fs.fun=fs.rf,
-        method="svmLinear",
-        tuneLength=5
-    ),
-    ctree=list(
-        fs.fun=fs.tree,
-        method="ctree"
-    ),
-    cforest=list(
-        fs.fun=fs.rf,
-        method="cforest",
-        tuneLength=7
-    ),
-    c50=list(
-        fs.fun=fs.tree,
-        method="C5.0",
-        #tuneLength=5
-        tuneGrid=expand.grid(
-            trials=1, winnow=c(F), model="rules"),
-        control=C5.0Control(earlyStopping=F)
-    ),
-    c50t=list(
-        fs.fun=fs.tree,
-        method="C5.0",
-        tuneLength=4,
-        control=C5.0Control(earlyStopping=F)
-    ),
-    c50C=list(
-      fs.fun=fs.tree,
-      method="C5.0",
-      tuneLength=15,
-      control=C5.0Control(earlyStopping=F)
-    )
+    n.minobsinnode=3
+  ),
+  mlp=list(
+    fs.fun=fs.rf,
+    method="mlp"
+  ),
+  mda=list(
+    fs.fun=fs.rf,
+    method="mda"
+  ),
+  lmt=list(
+    fs.fun=fs.tree,
+    method="LMT"
+  ),
+  lboost=list(
+    fs.fun=fs.rf,
+    method="LogitBoost"
+  ),
+  j48=list(
+    fs.fun=fs.tree,
+    method="J48"
+  ),
+  jrip=list(
+    fs.fun=fs.tree,
+    method="JRip"
+  ),
+  svmLinear=list(
+    fs.fun=fs.svm,
+    method="svmLinear"
+  ),
+  rf=list(
+    fs.fun=fs.rf,
+    method="rf",
+    tuneLength=2
+  ),
+  rfC=list(
+    fs.fun=fs.rf,
+    method="rf",
+    tuneLength=15
+  ),
+  rf2=list(
+    fs.fun=fs.rf,
+    method="extraTrees",
+    tuneLength=1
+  ),
+  orf=list(
+    fs.fun=fs.rf,
+    method="ORFridge"
+  ),
+  rrf=list(
+    fs.fun=fs.rf,
+    method="RRFglobal",
+    tuneLength=3
+  ),
+  treebag=list(
+    fs.fun=fs.rf,
+    method="treebag"
+  ),
+  svmLinear=list(
+    fs.fun=fs.rf,
+    method="svmLinear",
+    tuneLength=5
+  ),
+  ctree=list(
+    fs.fun=fs.tree,
+    method="ctree"
+  ),
+  cforest=list(
+    fs.fun=fs.rf,
+    method="cforest",
+    tuneLength=7
+  ),
+  c50=list(
+    fs.fun=fs.tree,
+    method="C5.0",
+    #tuneLength=5
+    tuneGrid=expand.grid(
+      trials=1, winnow=c(F), model="rules"),
+    control=C5.0Control(earlyStopping=F)
+  ),
+  c50t=list(
+    fs.fun=fs.tree,
+    method="C5.0",
+    tuneLength=4,
+    control=C5.0Control(earlyStopping=F)
+  ),
+  c50C=list(
+    fs.fun=fs.tree,
+    method="C5.0",
+    tuneLength=15,
+    control=C5.0Control(earlyStopping=F)
+  )
 )
 
 # Common description
 common.desc <- list(
-    # Serialize models to models/ directory
-    save.path="models",
-    # Function to apply to the data frame
-    fs.fun=fs.all,
-    # Data
-    trControl=ctrl
+  # Serialize models to models/ directory
+  save.path="models",
+  # Function to apply to the data frame
+  fs.fun=fs.all,
+  # Data
+  trControl=ctrl
 )
