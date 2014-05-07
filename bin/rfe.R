8c7033e3dedf814cad42b0e42cc218b233a9f5d8
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-07 15:44:27 +0000

    check in rfe.R
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1149 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

---
+++ bin/rfe.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+library(caret)
+library(data.table)
+
+source("R/data.R")
+source("R/fs.R")
+
+dt <- data.frame(fs.all(dt.dmc$M1$train))
+
+set.seed(42)
+dt <- dt[sample(nrow(dt), 1000), ]
+
+zeroVar <- names(which(sapply(dt, function(x) length(unique(x)) == 1)))
+dt <- dt[!names(dt) %in% zeroVar]
+
+funcs <- caretFuncs
+funcs$fit <- function(a, b, first, last, ...) {
+    train(a, b, method = "gbm", tuneLength=1, ...)
+}
+
+rctrl <- rfeControl(functions = funcs, method="cv", number = 10,
+                    saveDetails = T, allowParallel = F)
+pred <- dt$returnShipment
+dt$returnShipment <- NULL
+
+set.seed(42)
+rfefit <- rfe(x=dt, y=pred, 
+              sizes = 1:12 * 5,
+              metric="Accuracy",
+              maximize = T,
+              rfeControl=rctrl)
+rfefit
+
+saveRDS(rfefit)
