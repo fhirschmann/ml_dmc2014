b166ec0db5a406007e8574536d7b7e7f08c27ccd
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-07 13:06:20 +0000

    use rfIter
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1128 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/fs.R
+++ bin/fs.R
@@ -5,23 +5,39 @@ library(data.table)
 source("R/data.R")
 source("R/fs.R")
 
-dt <- fs.all(dt.train)
+dt.train <- data.frame(fs.all(dt.dmc$T1$train))
+dt.test <- data.frame(fs.all(dt.dmc$T1$test))
+
+# Take a sample
 set.seed(42)
-dt <- dt[sample(nrow(dt), 100), ]
+dt.train <- dt.train[sample(nrow(dt.train), 100), ]
+zeroVar <- names(which(sapply(dt.train, function(x) length(unique(x)) == 1)))
+dt.train <- dt.train[, !names(dt.train) %in% zeroVar]
+
+set.seed(42)
+dt.test <- dt.test[sample(nrow(dt.test), 10), ]
+dt.test <- dt.test[, !names(dt.test) %in% zeroVar]
+
+# Outcome
+y.train <- dt.train$returnShipment
+y.test <- dt.test$returnShipment
+
+# Training Data
+x.train <- dt.train
+x.train$returnShipment <- NULL
+x.test <- dt.test
+x.test$returnShipment <- NULL
 
 funcs <- caretFuncs
 funcs$fit <- function(a, b, first, last, ...) {
     train(a, b, method = "gbm", tuneLength=1, ...) 
 }
 
-rctrl <- rfeControl(functions=funcs, method="cv", number=2,
-                    saveDetails=T, allowParallel=F)
-
-pred <- dt$returnShipment
-dt$returnShipment <- NULL
+rctrl <- rfeControl(functions=funcs, saveDetails=T,
+                    allowParallel=F)
 
 set.seed(42)
-rfefit <- rfe(x=dt, y=pred, sizes = 4*(3:6), 
-              metric="Accuracy", maximize=T, rfeControl=rctrl)
+rfefit <- rfeIter(x.train, y.train, x.test, y.test, sizes=4*(3:6),
+                  rfeControl=rctrl)
 rfefit
 saveRDS(rfefit, file="rfe.RData")
