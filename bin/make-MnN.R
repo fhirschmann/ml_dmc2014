a949984d3744d2e963f08db5b160358b262f4c7b
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-07 15:33:28 +0000

    add script for creating MnN_test.txt
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1142 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

---
+++ bin/make-MnN.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+
+source("R/data.R")
+
+m1.known <- dt.dmc$M1$test$customerID %in% unique(dt.dmc$M1$train$customerID)
+m2.known <- dt.dmc$M2$test$customerID %in% unique(dt.dmc$M2$train$customerID)
+m3.known <- dt.dmc$M3$test$customerID %in% unique(dt.dmc$M3$train$customerID)
+
+message(paste("Known in M1:", sum(m1.known) / length(m1.known)))
+message(paste("Known in M2:", sum(m2.known) / length(m2.known)))
+message(paste("Known in M3:", sum(m3.known) / length(m3.known)))
+
+dt.dmc$M1$test <- data.frame(dt.dmc$M1$test)
+dt.dmc$M2$test <- data.frame(dt.dmc$M2$test)
+dt.dmc$M3$test <- data.frame(dt.dmc$M3$test)
+
+write.csv(dt.dmc$M1$test[m1.known, c("orderItemID"), drop=F], file="eva/M11_test.txt", row.names=F)
+write.csv(dt.dmc$M1$test[!m1.known, c("orderItemID"), drop=F], file="eva/M10_test.txt", row.names=F)
+
+write.csv(dt.dmc$M2$test[m2.known, c("orderItemID"), drop=F], file="eva/M21_test.txt", row.names=F)
+write.csv(dt.dmc$M2$test[!m2.known, c("orderItemID"), drop=F], file="eva/M20_test.txt", row.names=F)
+
+write.csv(dt.dmc$M3$test[m3.known, c("orderItemID"), drop=F], file="eva/M31_test.txt", row.names=F)
+write.csv(dt.dmc$M3$test[!m3.known, c("orderItemID"), drop=F], file="eva/M30_test.txt", row.names=F)
