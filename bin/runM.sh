00e933017804fc9e268a064af4469b28f16e7cd0
MichaelMarkert <MichaelMarkert@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-08 10:41:41 +0000

    Redirect output of runM.sh
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1201 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/runM.sh
+++ bin/runM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./bin/run.R gbmM1 M1 $*
-./bin/run.R gbmM10 M10 $*
-./bin/run.R gbmM11 M11 $*
+./bin/run.R gbmM1 M1 $* > M1.out 2>&1
+./bin/run.R gbmM10 M10 $* >  M10.out 2>&1
+./bin/run.R gbmM11 M11 $* > M11.out 2>&1
