1d9a95ba0169b92270b65f4187cf88e810167286
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-09 14:02:17 +0000

    fix arff export
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1293 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/export-arff.R
+++ bin/export-arff.R
@@ -10,7 +10,7 @@ for (n in names(dt.dmc)) {
     write.arff(train[train$deliveryDateMissing == "no", ],
                paste("arff/", n, "_noDDmissing_train.arff", sep=""))
 
-    write.arff(test, paste(n, "_test.arff", sep=""))
+    write.arff(test, paste("arff/", n, "_test.arff", sep=""))
     write.arff(test[test$deliveryDateMissing == "no", ],
                paste("arff/", n, "_noDDmissing_test.arff", sep=""))
 }
