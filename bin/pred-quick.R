dfaa9734b9ccf81bf56128d2b03d8e5f56c88e30
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-09 00:59:51 +0000

    write out preds
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1266 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/pred-quick.R
+++ bin/pred-quick.R
@@ -1,5 +1,6 @@
 #!/usr/bin/Rscript
 library(C50)
+library(plyr)
 
 source("R/data.R")
 source("R/fs.R")
@@ -18,23 +19,32 @@ for (s in c("M1", "M2", "M3")) {
         train <- dt.dmc[[sN]]$train
         train <- train[train$deliveryDateMissing == "no", ]
         train <- fs.c50(train, customerKnown)
+        #train <- train[1:1000, ]
         if (!customerKnown)
             train <- fs.noCustomer(train)
         train$deliveryDateMissing <- NULL
         
         test <- dt.dmc[[sN]]$test
+        orderItemIDmiss <- test[test$deliveryDateMissing == "yes", ]$orderItemID
         test <- test[test$deliveryDateMissing == "no", ]
+        orderItemID <- test$orderItemID
         test <- fs.c50(test, customerKnown)
         test$deliveryDateMissing <- NULL
         
         fit <- C5.0(returnShipment ~ ., data=train)
-        preds[[N+1]] <- predict(fit, test)
+        preds[[N+1]] <- data.frame(orderItemID=orderItemID,
+                                   prediction=revalue(predict(fit, test), c("yes"="1", "no"="0")))
         
-        scores[[N+1]] <- dmc.score(preds[[N+1]], test$returnShipment)
+        scores[[N+1]] <- dmc.score(preds[[N+1]]$prediction, test$returnShipment)
         
         message(paste("Score", sN, scores[[N+1]]))
         message(paste("Accuracy", 1 - scores[[N+1]] / nrow(dt.dmc[[sN]]$test)))
     }
     message(paste("Score", s, scores[[1]] + scores[[2]]))
     message(paste("Accuracy", s, 1 - (scores[[1]] + scores[[2]]) / nrow(dt.dmc[[s]]$test)))
+    
+    pred <- rbind(rbind(preds[[1]], preds[[2]]),
+                  data.frame(orderItemID=orderItemIDmiss,
+                             prediction="0"))
+    write.table(pred, file=paste(s, "_", "group3_binary.txt", sep=""), sep=";", quote=F, row.names=F)
 }
