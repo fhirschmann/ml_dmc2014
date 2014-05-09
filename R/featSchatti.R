67e603588fb8e9bb63fc6ad074f89d7461cdea33
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-09 13:36:18 +0000

    extend featSchatti
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1290 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/featSchatti.R
+++ R/featSchatti.R
@@ -7,11 +7,11 @@
 #
 # Unter Windows (zuerst install.packages("C50") in R ausführen):
 #
-# Rscript bin\fe.R c50 M30 nameDesFeatures
+#   Rscript bin\fe.R c50 M30 nameDesFeatures
 #
 # und
 #
-# Rscript bin\fe.R c50 M31 nameDesFeatures
+#   Rscript bin\fe.R c50 M31 nameDesFeatures
 #
 # Dann wird zuerst ein Modell mit allen Features gelernt (die erste Score), danach
 # wird ein Modell gelernt bei dem `nameDesFeatures` weggelassen wird. Ist die Score
@@ -19,11 +19,19 @@
 # und in diesem Fall würde es "Feature Kept" heißen. Eine sehr gute Änderung
 # wäre z.B. -50.
 
+# Um zu schauen ob die Features einigermaßen passen, kann man z.B. folgendes machen:
+#
+# Hier auf `Source` klicken, dann
+#
+#   source("R/data.R")
+#   x <- add.features.schatti(dt.train)
+#   str(x)
+
+
 add.features.schatti <- function(dt) {
     # Hier können Features hinzugefügt werden, die einfach nur an das vorgegebene
     # Train und Test Set gejoined werden oder auf einem Set berechnet werden
     # können ohne Einflussnahme des jeweils anderen
-    
     require(plyr)
     
     dt2 <- dt
@@ -36,11 +44,10 @@ add.features.schatti <- function(dt) {
     dt2 <- join(dt2, category, by=c("manufacturerID", "itemID"))
     dt2 <- join(dt2, categorySize, by=c("manufacturerID", "itemID", "itemSize"))
     
+    # Don't remove the next lines
     nrow.after <- nrow(dt2)
-    
     if (nrow.before != nrow.after)
         stop("Something went wrong with the join. :(")
-    
     dt2
 }
 
@@ -53,8 +60,9 @@ add.features.schatti.all <- function(dt, alldata) {
     
     
     
-    
+    # Don't remove the next lines
+    nrow.after <- nrow(dt2)
     if (nrow.before != nrow.after)
         stop("Something went wrong with the join. :(")
-    dt
+    dt2
 }
