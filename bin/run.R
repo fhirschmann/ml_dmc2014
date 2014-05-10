73c34fee4783d5f71304c9d56c7b85ef8752c640
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-10 01:40:46 +0000

    update node name
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1349 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/run.R
+++ bin/run.R
@@ -80,6 +80,6 @@ warnings()
 
 # Upload results
 
-if ((Sys.getenv("USER") %in% c("io93bafi", "ex66pimo")) | (Sys.info()["nodename"] == "ip-172-31-37-90")) {
+if ((Sys.getenv("USER") %in% c("io93bafi", "ex66pimo")) | (Sys.info()["nodename"] == "ip-172-31-7-33")) {
     system("./bin/sync-cluster.sh")
 }
