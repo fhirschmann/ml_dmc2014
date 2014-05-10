0861d6178d5291d6530638e686792b6e1b5b8637
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-10 16:00:55 +0000

    update sync script
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1375 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/sync-cluster.sh
+++ bin/sync-cluster.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-rsync --update -avz -e ssh models/* dmc@0x0b.de:/home/dmc/models/$USERNAME/
+TO=/home/dmc/models/$USER/
+echo "Syncing to $TO"
+
+rsync --update -avz -e ssh models/* dmc@0x0b.de:$TO
