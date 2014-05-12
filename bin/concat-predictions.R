647898dc8184b72492c972e4db797c5183b4e66e
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-12 11:32:56 +0000

    update concat pred
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1494 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- bin/concat-predictions.R
+++ bin/concat-predictions.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 #
 # Usage:
-#   ./bin/concat-predictions models c50C ensemble/M/M3_group3_c50_binary.txt
-#   ./bin/concat-predictions models c50C ensemble/M/M3_group3_c50_confidence.txt -p
+#   ./bin/concat-predictions c50_M30_pred.txt c50_M31_pred.txt ensemble/M/M3_group3_c50_binary.txt
+#   ./bin/concat-predictions c50_M30_prob.txt c50_M31_prob.txt ensemble/M/M3_group3_c50_confidence.txt
 
 args <- commandArgs(T)
 
@@ -11,10 +11,9 @@ probs <- "-p" %in% args
 
 suffix <- if ("-p" %in% args) "_prob" else "_pred"
 
-dir <- args[[1]]
-p1 <- args[[2]]
-p2 <- args[[3]]
-dest <- args[[4]]
+p1 <- args[[1]]
+p2 <- args[[2]]
+dest <- args[[3]]
 
 #p1 <- paste(file.path(dir, paste(name, "_", "M30", suffix, ".txt", sep="")))
 #p2 <- paste(file.path(dir, paste(name, "_", "M31", suffix, ".txt", sep="")))
