91f0b94c2f701b625ccd64422e00b72aff5f7a3d
FabianHirschmann <FabianHirschmann@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-12 11:42:44 +0000

    merge
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1500 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

--- R/pipeline.R
+++ R/pipeline.R
@@ -196,6 +196,14 @@ descs <- list(
         method="bagEarth",
         tuneGrid=expand.grid(
             degree=2,
+            nprune=7:14
+        )
+    ),
+    bearthM30=list(
+        fs.fun=fs.stat,
+        method="bagEarth",
+        tuneGrid=expand.grid(
+            degree=2,
             nprune=14:20
         )
     ),
