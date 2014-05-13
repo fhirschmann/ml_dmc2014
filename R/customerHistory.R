530f2865cce3fba7d9da9f856de2d09e71e70353
sholthausen <sholthausen@3bb05a21-cac8-4fc5-993d-a39f5ef0ea84>
2014-05-13 03:07:49 +0000

    bitte anschauen und finishen ... :(
    git-svn-id: https://www.ke.informatik.tu-darmstadt.de/svn/DMC14/R/trunk@1568 3bb05a21-cac8-4fc5-993d-a39f5ef0ea84

---
+++ R/customerHistory.R
@@ -0,0 +1,63 @@
+
+source("r/data.r")
+library(data.table)
+library(plyr)
+x <- dt.dmc$M30$train[sample(nrow(dt.dmc$M30$train), 2)]
+
+
+
+#dt.from <- data.table(x[x$deliveryDateMissing == "no", ])
+
+
+#orderDates <- unique(dt.from[, c("customerID", "orderDate", "itemID"), with=F])
+#setkeyv(orderDates, c("customerID", "orderDate", "itemID"))
+#orderDates$C <- paste(orderDates$orderDate, orderDates$customerID, sep="")
+##orderDates[, index := 1:.N, by=c("customerID")]
+#y <- orderDates[orderDates$customerID == 6]
+#mergedItems <- aggregate(itemID ~ C, y, as.vector)
+#y$itemID <- NULL
+#almostthere <- join(y, mergedItems, by=c("C"))
+
+customerList <- c()
+customerItemList <- c()
+customerOrderList <- c()
+customerSession <- c()
+
+str(customerItemList)
+
+yesorno <- function(customer, item, order) {
+  str("incoming")
+  str(customer)
+ # message("laosdaosdoas")
+  if(customer %in% customerList) {
+    if(!(order %in% customerOrderList[[customer]])) {
+      customerOrderList[[customer]] <<- c(customerOrderList[[customer]], order)
+      customerSession[[customer]] <<- NULL
+    }
+    if(!(item %in% customerSession[[customer]]) & (item %in% customerItemList[[customer]])) {
+      customerSession[[customer]] <<- c(customerSession[[customer]], item)
+      TRUE
+    } else if (item %in% customerSession[[customer]]){
+      str("item in customersession")
+      FALSE
+    } else {
+      str("item not yet in itemlist")
+      customerItemList[[customer]] <<- c(customerItemList[[customer]], item)
+      FALSE
+    }
+  } else {
+    str(item)
+    str("and the list")
+    str(customerItemList)
+    str(customerList)
+    customerList <<- c(customerList, customer)
+    customerItemList[[customer]] <<- c(customerItemList[[customer]], item)
+    customerSession[[customer]] <<- c(item)
+    customerOrderList[[customer]] <<- c(list(order), customerOrderList[[customer]] )
+    FALSE
+  }
+}
+
+for(i in 1:nrow(x)) {
+  x[i, "test2"] <- yesorno(x[i , ]$customerID, x[i , ]$itemID, x[i , ]$orderDate)
+}
